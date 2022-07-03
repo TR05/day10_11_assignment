f1e0ef5c62719a43cba6162e9e1de4cb1565a702
TR05 <tanyaraut2000@gmail.com>
2022-07-03 17:08:57 +0530

    Added UC1 and UC2

---
+++ UC1.sh
@@ -0,0 +1,5 @@
+read -p "Enter first input: " a
+read -p "Enter second input: " b
+read -p "Enter third input: " c
+
+echo $a $b $c
---
+++ UC2.sh
@@ -0,0 +1,6 @@
+read -p "Enter first input: " a
+read -p "Enter second input: " b
+read -p "Enter third input: " c
+
+ruc2=$(($a+$b*$c))
+echo $ruc2
